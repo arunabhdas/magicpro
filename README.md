9880036966678b57db9e6e33845f9c1364ac5923
Arunabh Das <arunabhdas@gmail.com>
2019-04-24 15:56:42 -0400

    Added MagicProCore to MagicPro dependency

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # magicpro
+
+## Links
+
+magic-pro-ios was created using Xcodegen 
+
+https://www.number42.de/blog/2018/07/24/xcodegen-article.html
+
---
+++ magicpro-ios/MagicPro/ItemViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ItemViewController.swift
+//  MagicPro
+//
+//  Created by Anonymous on 4/24/19.
+//
+
+import Foundation
+import UIKit
+
+
+
+class ItemViewController: UIViewController {
+  //MARK: UIViewController lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("-----\(Item.first)----")
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    
+  }
+  
+}
